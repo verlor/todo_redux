ed20fed0012725ad746ce777209dcf48aacd07f4
Noe Aguilar <neseiza@gmail.com>
2020-04-22 22:04:33 -0500

    paginita

---
+++ precache-manifest.abf71f418bd7123f0ccb35b00039bd16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87bfc4b69c7d7ffbc695986095f98a3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d805357e5a3adc890880",
+    "url": "/static/css/2.6917426b.chunk.css"
+  },
+  {
+    "revision": "d805357e5a3adc890880",
+    "url": "/static/js/2.2da71f7c.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/static/js/2.2da71f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da7555544dd755ae2e8",
+    "url": "/static/js/main.6f6a0b1c.chunk.js"
+  },
+  {
+    "revision": "d36bbc17440f8fd25332",
+    "url": "/static/js/runtime-main.ef674203.js"
+  }
+]);
